b4675aaadffaa1c271465b6a0b7dfd4af04afa3f
Nodirkhan <abdumurodovnodirxon@gmail.com>
2022-08-31 23:57:41 +0500

    Quick Sort

--- SortAlgorithms/Program.cs
+++ SortAlgorithms/Program.cs
@@ -4,11 +4,12 @@ namespace SortAlgorithms
 {
     internal class Program
     {
-        public static int[] Array = new int[] { 1, 2, 668, 5, 9 };
+        static int[] Array = new int[] { 73, 57, 49, 99, 133, 20, 1 };
         static void Main(string[] args)
         {
             //var result = SelectionSort.SelectionSortAlgorithms(Array);
-            var result = LinerSort.Algorithm(Array);
+            //var result = LinerSort.Algorithm(Array);
+            var result = QuickSort.SortArray(Array, 0, Array.Length -1 );
 
             foreach(int i in result)
             {
---
+++ SortAlgorithms/QuickSort.cs
@@ -0,0 +1,37 @@
+﻿namespace SortAlgorithms
+{
+    public static class QuickSort                       //73
+    {
+        // static int[] Array = new int[] { 73, 57, 49, 99, 133, 20, 1 };
+        public static int[] SortArray(int[] array, int leftIndex, int rightIndex)
+        {
+            int i = leftIndex;
+            int j = rightIndex;
+            int pivot = array[i];
+            while(i <= j)
+            {
+                while(array[i] < pivot)
+                {
+                    i++;
+                }
+                while(array[j] > pivot)
+                {
+                    j--;
+                }
+                if(i <= j)
+                {
+                    var tempvar = array[i];
+                    array[i] = array[j];
+                    array[j] = tempvar;
+                    i++;
+                    j--;
+                }
+            }
+            if (leftIndex < j)
+                SortArray(array, leftIndex,j);
+            if(rightIndex > i) 
+                SortArray(array, i, rightIndex);
+            return array;
+        }
+    }
+}
